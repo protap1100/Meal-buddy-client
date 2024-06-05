2063976108c33c1901fbcb8f32d0cd640a4b4bd8
protap1100 <protapb23@gmail.com>
2024-06-06 00:35:48 +0600

    use cart

---
+++ src/Hooks/useCart.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
+import useAuth from "./useAuth";
+
+const useCart = () => {
+
+    const axiosPublic = useAxiosPublic();
+    const {user} = useAuth();
+    
+    const {data: meal =[] ,isPending : loading, refetch} = useQuery({
+        queryKey: ['meal'],
+        queryFn : async() =>{
+          const res = await axiosPublic.get(`sServingMeals/${user?.email}`);
+          return res.data;
+        }
+      })
+    
+    
+      return [meal,loading,refetch]
+};
+
+export default useCart;
--- src/Pages/UserDashBoard.jsx/UserCart/UserCart.jsx
+++ src/Pages/UserDashBoard.jsx/UserCart/UserCart.jsx
@@ -1,6 +1,15 @@
 import SectionTitle from "../../../Components/Shared/SectionTitle";
+import useCart from "../../../Hooks/useCart";
+import Loading from "../../../Others/Loading";
 
 const UserCart = () => {
+
+    const [meal,loading,refetch] = useCart();
+
+    console.log(meal)
+    if(loading){
+        return <Loading></Loading>
+    }
     return (
         <div>
             <SectionTitle heading='Your Cart Item' subHeading='Item You Have Sended Request'></SectionTitle>
